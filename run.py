ec7238ae1b108b5b602da3ca962541f4ae7f78c8
Neznaev Sergey <sneznaev@gmail.com>
2025-01-01 15:34:35 +0800

    ошибка запуска main.py вместо run.py1

--- run.py
+++ run.py
@@ -23,8 +23,9 @@ def create_app():
 
     return app
 
+app = create_app()
+    
 if __name__ == "__main__":
-    app = create_app()
     if Config.FLASK_ENV == "development":
         logger.info("Запуск приложения в режиме разработки.")
         app.run(host="127.0.0.1", port=5000, debug=True)
